f1ddf81455ab79ceb4cdc1aeb334ee53a8b36aba
AAAkater <1318382761@qq.com>
2024-07-11 13:54:12 +0800

    🔧 build: add Antd

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import "@/assets/main.css"
 
 import { createApp } from "vue"
-import pinia from "@/stores/"
+import pinia from "@/stores"
 import router from "@/router"
 import App from "./App.vue"
-
+import Antd from "ant-design-vue"
+import "ant-design-vue/dist/reset.css"
 const app = createApp(App)
-
+app.use(Antd)
 app.use(pinia)
 app.use(router)
 
