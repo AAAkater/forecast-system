8e1387311461474b68b9027420f0c7547c3cd910
AAAkater <1318382761@qq.com>
2024-07-16 22:03:54 +0800

    🔧 build: add headless/ui

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import "@/assets/main.css"
 
-import { createApp } from "vue"
-import pinia from "@/stores"
 import router from "@/router"
-import App from "./App.vue"
+import pinia from "@/stores"
 import Antd from "ant-design-vue"
 import "virtual:svg-icons-register"
+import { createApp } from "vue"
+import App from "./App.vue"
 // import "ant-design-vue/dist/reset.css"
 const app = createApp(App)
 app.use(Antd)
