d380579fd3a107a9b4f09ac24a4eb81f9ff263d8
AAAkater <1318382761@qq.com>
2024-08-07 18:15:22 +0800

    ✨ feat: add title

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,37 @@
 <script setup lang="ts">
-import { message } from "ant-design-vue"
-import { onMounted } from "vue"
+// import { message } from "ant-design-vue"
+// import { onMounted } from "vue"
 
-onMounted(() => {
-  message.info("登录")
-})
+import LeftTitle from "./src/LeftTitle.vue"
+import RightForm from "./src/RightForm.vue"
 </script>
 
 <template>
-  <div>home</div>
+  <div
+    :class="['block h-screen w-full overflow-auto scrollbar-hide', 'md:flex']"
+  >
+    <video
+      :class="[
+        'z-[-1] h-screen w-full object-cover',
+        'absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]',
+      ]"
+      autoplay
+      loop
+      muted
+    >
+      <source
+        src="@/assets/video/bg.mp4"
+        type="video/mp4"
+      />
+    </video>
+
+    <div :class="['h-1/3 w-full', 'md:h-screen md:w-1/2']">
+      <LeftTitle />
+    </div>
+    <div :class="['h-2/3 min-h-[800px] w-full', 'md:h-screen md:w-1/2']">
+      <RightForm />
+    </div>
+  </div>
 </template>
 
 <style scoped lang="less"></style>
---
+++ src/views/login/src/RightForm.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="flex h-full w-full items-center justify-center">
+    <div class="h-[500px] w-4/5 max-w-96 rounded-lg bg-white/80 shadow-xl">
+      <img
+        src="@/assets/icons/logo.svg"
+        class="h-14 w-auto"
+      />
+      <div>欢迎回来</div>
+      <div>请输入登录信息</div>
+      <p>© 2024数据监控与实时检验终端 蜀ICP备2021022624号-1</p>
+    </div>
+  </div>
+</template>
+<style lang="less" scoped></style>
