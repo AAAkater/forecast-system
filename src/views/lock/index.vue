dbf8ee4bdf4576858f505cf51fda3d8a8da844fc
AAAkater <1318382761@qq.com>
2024-08-08 14:56:05 +0800

    ✨ feat: add lock page

--- src/views/lock/index.vue
+++ src/views/lock/index.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
-import { message } from "ant-design-vue"
-import { onMounted } from "vue"
-
-onMounted(() => {
-  message.info("锁屏")
-})
+import LockForm from "./src/LockForm.vue"
 </script>
 
 <template>
-  <div>home</div>
+  <div class="flex h-screen w-full items-center justify-center">
+    <video
+      :class="[
+        'z-[-1] h-screen w-full object-cover',
+        'absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]',
+      ]"
+      autoplay
+      loop
+      muted
+    >
+      <source
+        src="@/assets/video/lock.mp4"
+        type="video/mp4"
+      />
+    </video>
+    <LockForm />
+  </div>
 </template>
 
 <style scoped lang="less"></style>
