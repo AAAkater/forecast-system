9910fd97035635db2f8c90cce31813e80a24ad34
AAAkater <1318382761@qq.com>
2024-09-08 22:44:45 +0800

    ✨ feat: add home page

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,53 +1,20 @@
 <script lang="ts" setup>
-import { ref } from "vue"
-
-import { useRouter } from "vue-router"
-const router = useRouter()
-
-const items = ref([
-  {
-    name: "主页",
-    click: () => {
-      router.push("/dashboard/home")
-    },
-  },
-  {
-    name: "设备布局",
-    click: () => {
-      router.push("/dashboard/device")
-    },
-  },
-  {
-    name: "多源融合",
-    click: () => {
-      router.push("/dashboard/multiple")
-    },
-  },
-  {
-    name: "格点预报",
-    click: () => {
-      router.push("/dashboard/forecast")
-    },
-  },
-  {
-    name: "实时监测",
-    click: () => {
-      router.push("/dashboard/tables")
-    },
-  },
-])
+import EarthMap from "./src/EarthMap.vue"
+import InfoButton from "./src/InfoButton.vue"
+import NavigationMenu from "./src/NavigationMenu.vue"
+import StarsBackground from "./src/StarsBackground.vue"
 </script>
 <template>
-  <div class="h-screen w-full">
-    <ul class="p-0">
-      <li
-        class="my-3 list-none"
-        v-for="(item, idx) of items"
-        :key="idx"
-      >
-        <button @click="item.click">{{ item.name }}</button>
-      </li>
-    </ul>
+  <div
+    :class="[
+      'flex h-screen w-full items-center justify-center',
+      `bg-[url('@/assets/home_bg.jpg')] bg-[length:100%_100%] bg-no-repeat`,
+    ]"
+  >
+    <EarthMap />
+    <NavigationMenu />
+    <StarsBackground />
+    <InfoButton />
   </div>
 </template>
 <style lang="less" scoped></style>
